a8a506d8905c56b2511f68492acc16a01b3e4dfb
Thieu Nguyen <thieu@bluegrassdigital.com>
2020-09-18 17:09:42 +0700

    Show list

--- src/containers/CaseStudyList/index.tsx
+++ src/containers/CaseStudyList/index.tsx
@@ -4,8 +4,21 @@ const CaseStudyList = () => {
   return (
     <div id="container">
       <h1>Case Study List</h1>
+      <NumberList />
     </div>
   );
 }
 
 export default CaseStudyList;
+
+
+function NumberList() {
+  const listItems = [1, 2, 3, 4, 5, 6, 7].map((number) =>
+    <li key={number.toString()}>
+      {number}
+    </li>
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
+}
