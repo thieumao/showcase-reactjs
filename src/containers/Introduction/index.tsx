88cbd6cfc4ecbc0287cd56646a28e9871653c971
Thieu Nguyen <thieu@bluegrassdigital.com>
2020-09-18 15:36:51 +0700

    Horizontal Menu

--- src/containers/Introduction/index.tsx
+++ src/containers/Introduction/index.tsx
@@ -4,6 +4,9 @@ const Introduction = () => {
   return (
     <div>
       <h1>Introduction</h1>
+      <p>Transforming Digital into Business Solutions</p>
+      <p>We merge technical depth and business strategy to tailor-make digital products that turn innovation into your growth and profit.</p>
+      <b>TAP TO SEE OUR WORK</b>
     </div>
   );
 }
